a50089c859c7c7de15d2c087d04da410dbcde30e
Lyudmila17 <merkel.naller@gmail.com>
2018-06-19 21:54:21 +0200

    athene: custom halium f2fs support

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,6 +57,7 @@ TARGET_KERNEL_CONFIG := halium_athene_defconfig
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8952
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
+BOARD_USE_LOCAL_INITRD := true
 
 # Audio
 AUDIO_FEATURE_ENABLED_KPI_OPTIMIZE := true
